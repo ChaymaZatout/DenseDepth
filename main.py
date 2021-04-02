f024c242adb1182abebdf03e513f6add6f6b6fd3
chim <zatout.chayma@gmail.com>
2021-04-02 13:55:33 +0100

    compute point cloud (to fix).

--- main.py
+++ main.py
@@ -4,44 +4,42 @@ import torch
 from model import PTModel
 from PIL import Image
 import time
+from skimage.transform import resize
+from utils import predict, worldCoords, posFromDepth
 
+if __name__ == '__main__':
+    # load model:
+    pytorch_model = PTModel().float()
+    checkpoint = torch.load("nyu.pth.tar", map_location=torch.device('cpu'))
+    pytorch_model.load_state_dict(checkpoint['state_dict'])
+    pytorch_model.eval()
 
-def my_DepthNorm(x, maxDepth):
-    return maxDepth / x
+    # Input images:
+    file = "_in/sample_2.jpg"
+    img = np.asarray(Image.open(file), dtype='float32')
+    rgb_height, rgb_width = img.shape[:2]
+    xx, yy = worldCoords(width=rgb_width // 2, height=rgb_height // 2)
+    input = np.clip(img / 255, 0, 1)
 
+    # Compute results
+    start = time.time()
+    output = predict(pytorch_model, input)
+    print(f"Predicted in {time.time() - start} s.")
 
-def my_predict(model, image, minDepth=10, maxDepth=4000):
-    with torch.no_grad():
-        pytorch_input = torch.from_numpy(input).permute(2, 0, 1).unsqueeze(0)
-        # Compute predictions
-        predictions = model(pytorch_input)
-        # Put in expected range
-    return (np.clip(my_DepthNorm(predictions.numpy(), maxDepth=maxDepth), minDepth, maxDepth) / maxDepth)[0][0]
+    # Compute PCD and visualize:
+    # output = resize(np.load('demo_depth.npy'), (240,320))# original demo
+    pcd = posFromDepth(output.copy(), xx, yy)
+    pcd = pcd.astype('float32')
 
+    import open3d as o3d
+    pcl = o3d.geometry.PointCloud()
+    pcl.points = o3d.utility.Vector3dVector(pcd)
+    o3d.visualization.draw_geometries([pcl])
 
-# load model:
-pytorch_model = PTModel().float()
-checkpoint = torch.load("nyu.pth.tar", map_location=torch.device('cpu'))
-pytorch_model.load_state_dict(checkpoint['state_dict'])
-pytorch_model.eval()
+    # save image:
+    # im = Image.fromarray(np.uint8(output * 255))
+    # im.save("_out/sample_2_depth.png")
 
-# Input images:
-file = "_in/sample.jpg"
-input = np.clip(np.asarray(Image.open(file), dtype='float32') / 255, 0, 1)
-
-# Compute results
-start = time.time()
-output = my_predict(pytorch_model, input)
-print(f"Predicted in {time.time() - start} s.")
-
-print(output.shape)
-print(f"min: {output.min()}")
-print(f"max: {output.max()}")
-
-# # save image:
-# im = Image.fromarray(np.uint8(output[0, 0, :, :] * 255))
-# im.save("_out/sample_depth.png")
-
-# # display:
-# plt.imshow(output[0, 0, :, :])
-# plt.show()
+    # display:
+    # plt.imshow(output)
+    # plt.show()
