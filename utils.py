7c94fd64d1159dc73b0f643a250f3c448b30e58b
chim <zatout.chayma@gmail.com>
2021-04-02 13:46:14 +0100

    Added to compute pcd.

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 import torch
 
+
 def DepthNorm(x, maxDepth):
     return maxDepth / x
 
@@ -15,29 +16,41 @@ def predict(model, image, minDepth=10, maxDepth=1000):
     return (np.clip(DepthNorm(predictions.numpy(), maxDepth=maxDepth), minDepth, maxDepth) / maxDepth)[0][0]
 
 
+def to_pcd(depth, cx_d, fx_d, cy_d, fy_d):
+    mat = np.array([[_ for _ in range(depth.shape[0])] for _ in
+                    range(depth.shape[1])])
+    x = (mat.transpose() - cx_d) * depth / fx_d
+    y = -((np.array([[_ for _ in range(depth.shape[1])] for _ in
+                     range(depth.shape[0])]) - cy_d) * depth / fy_d)
+    return np.column_stack((x.flatten(), y.flatten(), depth.flatten()))
+
+
 def scale_up(scale, images):
     from skimage.transform import resize
     scaled = []
-    
+
     for i in range(len(images)):
         img = images[i]
         output_shape = (scale * img.shape[0], scale * img.shape[1])
-        scaled.append( resize(img, output_shape, order=1, preserve_range=True, mode='reflect', anti_aliasing=True ) )
+        scaled.append(resize(img, output_shape, order=1, preserve_range=True, mode='reflect', anti_aliasing=True))
 
     return np.stack(scaled)
+
 
 def load_images(image_files):
     loaded_images = []
     for file in image_files:
-        x = np.clip(np.asarray(Image.open( file ), dtype=float) / 255, 0, 1)
+        x = np.clip(np.asarray(Image.open(file), dtype=float) / 255, 0, 1)
         loaded_images.append(x)
     return np.stack(loaded_images, axis=0)
 
+
 def to_multichannel(i):
     if i.shape[2] == 3: return i
-    i = i[:,:,0]
-    return np.stack((i,i,i), axis=2)
-        
+    i = i[:, :, 0]
+    return np.stack((i, i, i), axis=2)
+
+
 def display_images(outputs, inputs=None, gt=None, is_colormap=True, is_rescale=True):
     import matplotlib.pyplot as plt
     import skimage
@@ -46,28 +59,28 @@ def display_images(outputs, inputs=None, gt=None, is_colormap=True, is_rescale=T
     plasma = plt.get_cmap('plasma')
 
     shape = (outputs[0].shape[0], outputs[0].shape[1], 3)
-    
+
     all_images = []
 
     for i in range(outputs.shape[0]):
         imgs = []
-        
+
         if isinstance(inputs, (list, tuple, np.ndarray)):
             x = to_multichannel(inputs[i])
-            x = resize(x, shape, preserve_range=True, mode='reflect', anti_aliasing=True )
+            x = resize(x, shape, preserve_range=True, mode='reflect', anti_aliasing=True)
             imgs.append(x)
 
         if isinstance(gt, (list, tuple, np.ndarray)):
             x = to_multichannel(gt[i])
-            x = resize(x, shape, preserve_range=True, mode='reflect', anti_aliasing=True )
+            x = resize(x, shape, preserve_range=True, mode='reflect', anti_aliasing=True)
             imgs.append(x)
 
         if is_colormap:
-            rescaled = outputs[i][:,:,0]
+            rescaled = outputs[i][:, :, 0]
             if is_rescale:
                 rescaled = rescaled - np.min(rescaled)
                 rescaled = rescaled / np.max(rescaled)
-            imgs.append(plasma(rescaled)[:,:,:3])
+            imgs.append(plasma(rescaled)[:, :, :3])
         else:
             imgs.append(to_multichannel(outputs[i]))
 
@@ -75,24 +88,27 @@ def display_images(outputs, inputs=None, gt=None, is_colormap=True, is_rescale=T
         all_images.append(img_set)
 
     all_images = np.stack(all_images)
-    
-    return skimage.util.montage(all_images, multichannel=True, fill=(0,0,0))
+
+    return skimage.util.montage(all_images, multichannel=True, fill=(0, 0, 0))
+
 
 def save_images(filename, outputs, inputs=None, gt=None, is_colormap=True, is_rescale=False):
-    montage =  display_images(outputs, inputs, is_colormap, is_rescale)
-    im = Image.fromarray(np.uint8(montage*255))
+    montage = display_images(outputs, inputs, is_colormap, is_rescale)
+    im = Image.fromarray(np.uint8(montage * 255))
     im.save(filename)
+
 
 def compute_errors(gt, pred):
     thresh = np.maximum((gt / pred), (pred / gt))
-    a1 = (thresh < 1.25   ).mean()
+    a1 = (thresh < 1.25).mean()
     a2 = (thresh < 1.25 ** 2).mean()
     a3 = (thresh < 1.25 ** 3).mean()
     abs_rel = np.mean(np.abs(gt - pred) / gt)
     rmse = (gt - pred) ** 2
     rmse = np.sqrt(rmse.mean())
-    log_10 = (np.abs(np.log10(gt)-np.log10(pred))).mean()
+    log_10 = (np.abs(np.log10(gt) - np.log10(pred))).mean()
     return a1, a2, a3, abs_rel, rmse, log_10
+
 
 def evaluate(model, rgb, depth, crop, batch_size=6, verbose=False):
     N = len(rgb)
@@ -101,26 +117,28 @@ def evaluate(model, rgb, depth, crop, batch_size=6, verbose=False):
 
     predictions = []
     testSetDepths = []
-    
-    for i in range(N//bs):    
-        x = rgb[(i)*bs:(i+1)*bs,:,:,:]
-        
+
+    for i in range(N // bs):
+        x = rgb[(i) * bs:(i + 1) * bs, :, :, :]
+
         # Compute results
-        true_y = depth[(i)*bs:(i+1)*bs,:,:]
-        pred_y = scale_up(2, predict(model, x/255, minDepth=10, maxDepth=1000, batch_size=bs)[:,:,:,0]) * 10.0
-        
+        true_y = depth[(i) * bs:(i + 1) * bs, :, :]
+        pred_y = scale_up(2, predict(model, x / 255, minDepth=10, maxDepth=1000, batch_size=bs)[:, :, :, 0]) * 10.0
+
         # Test time augmentation: mirror image estimate
-        pred_y_flip = scale_up(2, predict(model, x[...,::-1,:]/255, minDepth=10, maxDepth=1000, batch_size=bs)[:,:,:,0]) * 10.0
+        pred_y_flip = scale_up(2,
+                               predict(model, x[..., ::-1, :] / 255, minDepth=10, maxDepth=1000, batch_size=bs)[:, :, :,
+                               0]) * 10.0
 
         # Crop based on Eigen et al. crop
-        true_y = true_y[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
-        pred_y = pred_y[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
-        pred_y_flip = pred_y_flip[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
-        
+        true_y = true_y[:, crop[0]:crop[1] + 1, crop[2]:crop[3] + 1]
+        pred_y = pred_y[:, crop[0]:crop[1] + 1, crop[2]:crop[3] + 1]
+        pred_y_flip = pred_y_flip[:, crop[0]:crop[1] + 1, crop[2]:crop[3] + 1]
+
         # Compute errors per image in batch
         for j in range(len(true_y)):
-            predictions.append(   (0.5 * pred_y[j]) + (0.5 * np.fliplr(pred_y_flip[j]))   )
-            testSetDepths.append(   true_y[j]   )
+            predictions.append((0.5 * pred_y[j]) + (0.5 * np.fliplr(pred_y_flip[j])))
+            testSetDepths.append(true_y[j])
 
     predictions = np.stack(predictions, axis=0)
     testSetDepths = np.stack(testSetDepths, axis=0)
@@ -129,6 +147,6 @@ def evaluate(model, rgb, depth, crop, batch_size=6, verbose=False):
 
     if verbose:
         print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format('a1', 'a2', 'a3', 'rel', 'rms', 'log_10'))
-        print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(e[0],e[1],e[2],e[3],e[4],e[5]))
+        print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(e[0], e[1], e[2], e[3], e[4], e[5]))
 
     return e
